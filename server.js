678f5d84644d9c233d7cf9a64157c3d6c70d40c5
SunLn <dengchenhua@gmail.com>
2014-01-06 18:10:53 +0800

    add server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+var qiniu = require('qiniu');
+var express = require('express');
+var app = express();
+
+app.configure(function() {
+    app.use(express.static(__dirname + '/public'));
+});
+
+app.get('/token', function(req, res, next) {
+    var token = uptoken.token();
+    if (token) {
+        res.json({
+            uptoken: token
+        })
+    }
+});
+
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/public/index.html')
+});
+
+qiniu.conf.ACCESS_KEY = '0MLvWPnyya1WtPnXFy9KLyGHyFPNdZceomLVk0c9';
+qiniu.conf.SECRET_KEY = 'o5itRgrXxoD6XQ5wDWKQ7h--eWvWyQVKcsIURuEV';
+
+var uptoken = new qiniu.rs.PutPolicy('nodejs-sdk-plupload');
+
+
+app.listen(3000);
+
+console.log(qiniu);
+
+console.log('server runing at localhost:' + 3000)
